45d0d1e988be6a27d4ece1e8371d76854db62d2e
Vanessa McHale <vamchale@gmail.com>
2020-01-27 20:27:47 -0600

    aaaaah

--- test-harness.py
+++ test-harness.py
@@ -1,10 +1,10 @@
 import imgfut
 
 import imageio
-from PIL import Image
+#  from PIL import Image
 import numpy as np
 
-from matplotlib import pyplot as plt
+#  from matplotlib import pyplot as plt
 
 mod = imgfut.imgfut()
 
@@ -12,11 +12,13 @@ img0 = np.array(imageio.imread('data/frog.png', pilmode='F'))
 
 edges_img0 = mod.sobel_f32(img0)
 blur_img0 = mod.mean_filter_f32(img0)
-print(img0)
-print(blur_img0)
 
-plt.gray()
-plt.imshow(edges_img0)
+from skimage import io
+io.imshow(blur_img0)
+io.show()
+
+#  plt.gray()
+#  plt.imshow(Image.fromarray(edges_img0))
 
 #  imageio.imwrite('frog-edges.png', Image.fromarray(np.array(edges_img0), 'F'))
 #  imageio.imwrite('frog-blur.png', Image.fromarray(np.array(blur_img0), 'F'))
