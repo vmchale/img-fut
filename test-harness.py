888b1785109cfaf64bb823dd04efc84c3868feb7
Vanessa McHale <vamchale@gmail.com>
2020-02-15 13:57:03 -0600

    lol test

--- test-harness.py
+++ test-harness.py
@@ -9,16 +9,23 @@ from matplotlib import pyplot as plt
 mod = imgfut.imgfut()
 
 img0 = np.array(imageio.imread('data/frog.png', pilmode='F'))
+img1 = np.array(imageio.imread('data/frog-squished.png', pilmode='F'))
 
 print ("processing...")
 edges_img0 = mod.sobel_f32(img0)
+edges_img0_scipy = scipy.ndimage.sobel(img0)
 blur_img0 = mod.mean_filter_f32(img0)
 gauss_img0 = mod.gaussian_filter_f32(img0)
 laplacian_img0 = mod.laplacian_filter_f32(img0)
 gauss_img0_scipy = scipy.ndimage.gaussian_filter(img0, 3)
 log_img0 = mod.laplacian_gaussian_filter_f32(img0)
+fft_blur_img1 = mod.fft_mean_filter_f32(img1)
+fft_blur_img1_scipy = scipy.ndimage.fourier_uniform(img1, 0)
 print ("done processing...")
 
 plt.gray()
-plt.imshow(log_img0.get())
+# lol this is inverted/wrong?
+plt.imshow(edges_img0.get())
+plt.show()
+plt.imshow(edges_img0_scipy)
 plt.show()
