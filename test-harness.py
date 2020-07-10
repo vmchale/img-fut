383a676966b476945d95abd9967d76921b6a2cb4
Vanessa McHale <vamchale@gmail.com>
2020-07-10 12:55:03 -0500

    Add datum; scipy sobel filter is weird idk

--- test-harness.py
+++ test-harness.py
@@ -10,9 +10,11 @@ mod = imgfut.imgfut()
 
 img0 = np.array(imageio.imread('data/frog.png', pilmode='F'))
 img1 = np.array(imageio.imread('data/frog-squished.png', pilmode='F'))
+img2 = np.array(imageio.imread('data/valve.png', pilmode='F'))
 
 print ("processing...")
 edges_img0 = mod.sobel_f32(img0)
+edges_img2 = mod.sobel_f32(img2)
 edges_img0_scipy = scipy.ndimage.sobel(img0)
 blur_img0 = mod.mean_filter_f32(img0)
 gauss_img0 = mod.gaussian_filter_f32(img0)
@@ -24,8 +26,8 @@ fft_blur_img1_scipy = scipy.ndimage.fourier_uniform(img1, 0)
 print ("done processing...")
 
 plt.gray()
-# lol this is inverted/wrong?
 plt.imshow(edges_img0.get())
 plt.show()
+# lol this is inverted/wrong?
 plt.imshow(edges_img0_scipy)
 plt.show()
